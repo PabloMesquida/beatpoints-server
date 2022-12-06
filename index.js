1608f8d172c197bf602a112605f27d7a04da927f
PabloMesquida <pablomesquida67@gmail.com>
2022-12-06 11:25:08 +0100

    URI

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const URI = "https://beatpoints.vercel.app";
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, (err) => {
   if (err) throw err;
@@ -18,7 +19,7 @@ app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   console.log("ok");
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
+    redirectUri: URI,
     clientId: "eafd5a23f1cb4f02b98c1cda9aa21333",
     clientSecret: "bb78d5a6ed1b4abfa8ec5f93731cf401",
     refreshToken,
@@ -43,7 +44,7 @@ app.post("/login", (req, res) => {
   const code = req.body.code;
   // console.log("code", code);
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
+    redirectUri: URI,
     clientId: "eafd5a23f1cb4f02b98c1cda9aa21333",
     clientSecret: "bb78d5a6ed1b4abfa8ec5f93731cf401",
   });
