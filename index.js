7bb6c6c67cc7acdc476230a816405ce4697327f6
PabloMesquida <pablomesquida67@gmail.com>
2022-12-06 12:31:33 +0100

    env

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const URI = "https://beatpoints.vercel.app";
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, (err) => {
   if (err) throw err;
@@ -19,34 +18,31 @@ app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   console.log("ok");
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: URI,
-    clientId: "eafd5a23f1cb4f02b98c1cda9aa21333",
-    clientSecret: "bb78d5a6ed1b4abfa8ec5f93731cf401",
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     refreshToken,
   });
 
   spotifyApi
     .refreshAccessToken()
     .then((data) => {
-      // console.log("refrehsData: ", data);
       res.json({
         accessToken: data.body.access_token,
         expiresIn: data.body.expires_in,
       });
     })
     .catch((err) => {
-      //  console.log(err);
       res.sendStatus(400);
     });
 });
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
-  // console.log("code", code);
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: URI,
-    clientId: "eafd5a23f1cb4f02b98c1cda9aa21333",
-    clientSecret: "bb78d5a6ed1b4abfa8ec5f93731cf401",
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
   });
 
   spotifyApi
@@ -59,7 +55,6 @@ app.post("/login", (req, res) => {
       });
     })
     .catch((err) => {
-      // console.log("post-login-error", err);
       res.sendStatus(400);
     });
 });
